dec8036250b5e4872b6992982d44e510f087f57b
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 09:49:49 -0700

    Add methods to format date and time for courses

--- src/CourseCard/CourseCard.js
+++ src/CourseCard/CourseCard.js
@@ -1,9 +1,23 @@
 import { Fragment } from 'react'
 
 const CourseCard = ({ id, name, days, time, students }) => {
+  const formatTime = (num) => {
+    if (num === 12) { return '12 PM' }
+    return num <= 12 ? `${num} AM` : `${num - 12} PM`
+  }
+
+  const formatDays = () => {
+    return days.reduce((summary, day) => {
+      summary += `${day} `
+      return summary
+    }, '')
+  }
+
   return (
     <Fragment>
       <h2>{ name }</h2>
+      <p> {formatTime(time[0])} - {formatTime(time[1])} </p>
+      <p> {formatDays()} </p>
     </Fragment>
   )
 }
