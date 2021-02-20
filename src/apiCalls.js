58c0485be5ce016b8f0483bb6cf36226f6d27239
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 12:31:59 -0700

    Connect student courses to profile

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,13 +3,22 @@ export const getCourses = () => {
   .then(res => res.json())
 }
 
+export const getCourseById = (id) => {
+  return fetch(`http://localhost:4000/courses/${id}`)
+    .then(res => res.json())
+}
+
 export const getStudents = () => {
   return fetch('http://localhost:4000/students')
     .then(res => res.json())
 }
 
+export const getStudent = (id) => {
+  return fetch(`http://localhost:4000/students/${id}`)
+    .then(res => res.json())
+}
+
 export const registerStudent = (courseId, studentId) => {
-  console.log(studentId)
   return fetch(`http://localhost:4000/courses/${courseId}/register`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
