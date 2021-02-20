48ac9c5fb99f78b8e9eea9c239ec5e09d164c9e6
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 10:12:52 -0700

    Add method to register student and connect to btn

--- src/CourseCard/CourseCard.js
+++ src/CourseCard/CourseCard.js
@@ -1,6 +1,10 @@
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
+import AppContext from '../App/AppContext'
+import { registerStudent } from '../apiCalls'
 
 const CourseCard = ({ id, name, days, time, students }) => {
+  const selectedStudent = useContext(AppContext)
+
   const formatTime = (num) => {
     if (num === 12) { return '12 PM' }
     return num <= 12 ? `${num} AM` : `${num - 12} PM`
@@ -13,12 +17,21 @@ const CourseCard = ({ id, name, days, time, students }) => {
     }, '')
   }
 
+  const handleRegistration = () => {
+    if (!selectedStudent) {
+      alert('Please select a student from the dropdown to register for this course')
+    } else {
+      let student = { id: selectedStudent }
+      registerStudent(id, student)
+    }
+  }
+
   return (
     <Fragment>
       <h2>{ name }</h2>
       <p> {formatTime(time[0])} - {formatTime(time[1])} </p>
       <p> {formatDays()} </p>
-      <button>Register</button>
+      <button onClick={() => handleRegistration()}>Register</button>
     </Fragment>
   )
 }
--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,3 +7,13 @@ export const getStudents = () => {
   return fetch('http://localhost:4000/students')
     .then(res => res.json())
 }
+
+export const registerStudent = (courseId, studentId) => {
+  console.log(studentId)
+  return fetch(`http://localhost:4000/courses/${courseId}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(studentId)
+  })
+    .then(response => response.json())
+}
