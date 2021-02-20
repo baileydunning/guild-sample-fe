3f9521113a0fee90ab9d402f92d2d77aa153790c
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 09:51:40 -0700

    Add method to GET students

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,8 @@ export const getCourses = () => {
   return fetch('http://localhost:4000/courses')
   .then(res => res.json())
 }
+
+export const getStudents = () => {
+  return fetch('http://localhost:4000/students')
+    .then(res => res.json())
+}
