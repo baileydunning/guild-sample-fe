be4898addf1e2952c216ae6399c91a2be4fef124
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 15:15:42 -0700

    Fix issue with mocked fetch value

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -12,20 +12,20 @@ jest.mock('../apiCalls')
 describe('App', () => {
   const history = createMemoryHistory()
   beforeEach(async() => {
-    act(async() => {
-      getCourses.mockResolvedValueOnce(courses)
-      getStudents.mockResolvedValueOnce(students)
+    await act(async() => {
+      getCourses.mockResolvedValue(courses)
+      getStudents.mockResolvedValue(students)
+      render(<Router history={history}><App /></Router>)
     })
-    render(<Router history={history}><App /></Router>)
   })
   
   it('should render the app', () => {
     const appHeader = screen.getByText('Course Catalog')
     expect(appHeader).toBeInTheDocument()
-    expect(history.path.url).toBe('/')
+    expect(history.location.pathname).toBe('/')
   })
   
   it('should display courses from fetch', () => {
-
+    screen.debug()
   })
 })
