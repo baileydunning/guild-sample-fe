a526ad4efb6a73aaa24f447ef5a1f583f4afc4cb
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 09:57:27 -0700

    Create dropdown menu to select from all students

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,27 @@
-const Header = () => {
+import { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = ({ students }) => {
+  const createStudentDropdown = () => {
+    return students.map(student => {
+      return (
+        <option
+          key={ student.id }
+          value={ student.id }>
+          { student.name }
+        </option>
+      )
+    })
+  }
+
   return (
-    <h1>Course Catalog</h1>
+    <header>
+      <Link to='/'><h1>Course Catalog</h1></Link>
+      <select>
+        <option value={''}>Select a student</option>
+        { createStudentDropdown() }
+      </select>
+    </header>
   )
 }
 
