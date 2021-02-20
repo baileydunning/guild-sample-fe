3b1e50641748913b016f1ae9cc2d83884071eef6
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 09:43:38 -0700

    Set up CourseCard component

---
+++ src/CourseCard/CourseCard.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+
+const CourseCard = ({ id, name, days, time, students }) => {
+  return (
+    <Fragment>
+      <h2>{ name }</h2>
+    </Fragment>
+  )
+}
+
+export default CourseCard
