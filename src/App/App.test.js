6b487b82a95c719a10298e20996170aad79c9dfc
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 15:02:25 -0700

    Set up api imports for testing

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,18 +1,31 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor, act } from '@testing-library/react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
+import userEvent from '@testing-library/user-event'
+import { getCourses, getStudents } from '../apiCalls'
+import { students, courses } from '../testData'
 import App from './App'
+jest.mock('../apiCalls')
 
 describe('App', () => {
   const history = createMemoryHistory()
-  beforeEach(() => {
+  beforeEach(async() => {
+    act(async() => {
+      getCourses.mockResolvedValueOnce(courses)
+      getStudents.mockResolvedValueOnce(students)
+    })
     render(<Router history={history}><App /></Router>)
   })
-
+  
   it('should render the app', () => {
     const appHeader = screen.getByText('Course Catalog')
     expect(appHeader).toBeInTheDocument()
+    expect(history.path.url).toBe('/')
+  })
+  
+  it('should display courses from fetch', () => {
+
   })
 })
