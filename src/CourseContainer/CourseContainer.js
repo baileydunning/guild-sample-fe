8a2a00862342902d19ede7e4394796e1f8e8961f
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 09:43:51 -0700

    Map over courses data to create cards

--- src/CourseContainer/CourseContainer.js
+++ src/CourseContainer/CourseContainer.js
@@ -1,4 +1,22 @@
-const CourseContainer = () => {
+import CourseCard from '../CourseCard/CourseCard'
+
+const CourseContainer = ({ courses }) => {
+
+  const createCourseCards = () => {
+    return courses.map(course => {
+      return (
+        <CourseCard 
+          key={course.id}
+          id={course.id}
+          name={course.name}
+          days={course.days}
+          time={course.time}
+          students={course.students}
+        />
+      )
+    })
+  }
+
   return (
     <p>this is where courses go</p>
   )
