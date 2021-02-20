8dc275fe9d7eaeb2fa37f9db27b67fee0ab69195
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 09:39:42 -0700

    Connect API to app to fetch courses

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import AppContext from './AppContext'
 import Header from '../Header/Header'
 import CourseContainer from '../CourseContainer/CourseContainer'
+import { getCourses } from '../apiCalls'
 import './App.scss'
 
 const App = () => {
+  const [courses, setCourses] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    getCourses()
+    .then(data => setCourses(data.allCourses))
+    .catch(err => setError(err))
+  }, [])
+
   return (
     <AppContext.Provider>
       <Header />
-      <CourseContainer />
+      <Switch>
+        <Route 
+          exact
+          path='/'
+          render={() =>
+            <CourseContainer courses={courses}/>
+          }
+        />
+      </Switch>
     </AppContext.Provider>
   )
 }
